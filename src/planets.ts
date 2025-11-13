dc242875fd2eadbd68a0bd6d33b2afb405869bcf
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2025-11-13 16:49:23 +0300

    Implement celestial object orbits

--- src/planets.ts
+++ src/planets.ts
@@ -1,4 +1,4 @@
-import { Mesh, SphereGeometry, PointLight, TextureLoader, Object3D, Points, Vector3, BufferGeometry, PointsMaterial, Float32BufferAttribute, MeshBasicMaterial } from "three";
+import { Mesh, SphereGeometry, PointLight, TextureLoader, Object3D, Points, Vector3, BufferGeometry, PointsMaterial, Float32BufferAttribute, MeshBasicMaterial, Quaternion, LineLoop, LineBasicMaterial } from "three";
 let textureLoader = new TextureLoader()
 textureLoader.setPath('/res/img/')
 function randomIntFromRange(min: number, max: number) {
@@ -8,54 +8,86 @@ function randomFloatFromRange(min: number, max: number) {
   return (Math.random() * (max - min)) + min
 }
 
-type CelestialObjectOptions = { 
+function transformVector3(orientation: Quaternion, vector: Vector3) {
+  const vx = vector.x,
+    vy = vector.y,
+    vz = vector.z
+  const qx = orientation.x,
+    qy = orientation.y,
+    qz = orientation.z,
+    qw = orientation.w
+
+  const tx = 2 * (qy * vz - qz * vy)
+  const ty = 2 * (qz * vx - qx * vz)
+  const tz = 2 * (qx * vy - qy * vx)
+
+  vector.x = vx + qw * tx + qy * tz - qz * ty
+  vector.y = vy + qw * ty + qz * tx - qx * tz
+  vector.z = vz + qw * tz + qx * ty - qy * tx
+
+  return vector
+}
+
+type CelestialObjectOptions = {
+  name?: string;
   rotationSpeed?: number;
   revolutionSpeed?: number;
   radius?: number;
   segments?: number;
+  distance?: number;
   textures: string[];
   wireframe: boolean;
-  distance: number;
 }
 class Sun extends Object3D {
+  orbitRotation = 0
   rotationSpeed: number;
   revolutionSpeed: number;
-  mesh: Object3D;
-  constructor(params: CelestialObjectOptions) {
+  distance: number;
+  constructor({
+    name,
+    textures,
+    revolutionSpeed = 0,
+    rotationSpeed = 0,
+    distance = 0,
+    segments,
+    radius,
+    wireframe
+  }: CelestialObjectOptions) {
     super()
     const light = new PointLight(0xf5e275, 10, 10000)
-    const mesh = new Mesh(new SphereGeometry(params.radius, params.segments, params.segments), new MeshBasicMaterial({
-      map: params.textures[0] ? textureLoader.load(params.textures[0]) : undefined,
-      wireframe: params.wireframe || false
+    const mesh = new Mesh(new SphereGeometry(radius, segments, segments), new MeshBasicMaterial({
+      map: textures[0] ? textureLoader.load(textures[0]) : undefined,
+      wireframe: wireframe || false
     }));
-    
-    mesh.position.set(params.distance, 0, 0)
+    this.position.set(distance, 0, 0)
     mesh.castShadow = true
     mesh.receiveShadow = true
     light.castShadow = true
 
-    this.name = 'sun'
-    this.rotationSpeed = params.rotationSpeed || 0
-    this.revolutionSpeed = params.revolutionSpeed || 0
-    this.mesh = mesh
+    this.name = name || 'celestial object'
+    this.distance = distance
+    this.rotationSpeed = rotationSpeed
+    this.revolutionSpeed = revolutionSpeed
     this.add(mesh)
   }
 
-  update() {
-    this.mesh.rotateY(this.rotationSpeed)
+  update(dt: number) {
+    if (this.parent) {
+      const inverse = this.parent.getWorldQuaternion(new Quaternion()).invert()
+      const distance = transformVector3(inverse, new Vector3(this.distance, 0, 0))
+
+      this.position.copy(distance)
+    }
+    this.orbitRotation += this.revolutionSpeed * dt
+    this.position.set(Math.sin(this.orbitRotation) * this.distance, 0, Math.cos(this.orbitRotation) * this.distance)
+    this.rotateY(this.rotationSpeed * dt)
   }
 }
 
 class Planet extends Sun {
-  constructor(params: { name?: string; revolutionSpeed?: number; rotationSpeed?: number; radius?: number; segments?: number; textures: string[]; wireframe: boolean; distance: number; }) {
+  constructor(params: CelestialObjectOptions) {
     super(params)
     this.name = `planet/${params.name}`
-  }
-
-  override update() {
-    this.rotateY(this.rotationSpeed)
-    this.mesh.rotateY(this.rotationSpeed)
-    super.update()
   }
 }
 
@@ -66,7 +98,6 @@ class Moon extends Planet {
   }
   override update() {
     this.rotateY(this.rotationSpeed)
-    this.mesh.rotateY(this.rotationSpeed)
   }
 }
 
@@ -74,7 +105,7 @@ class Asteriods extends Planet {
   constructor(params: { name?: string; revolutionSpeed?: number; rotationSpeed?: number; radius?: number; segments?: number; textures: string[]; wireframe: boolean; distance: number; }) {
     super(params)
     this.name = 'asteroids'
-    this.remove(this.mesh)
+    this.clear()
     this.initAsteroids(params)
   }
   initAsteroids(params: { name?: string | undefined; revolutionSpeed?: number | undefined; rotationSpeed?: number | undefined; radius?: number | undefined; segments?: number | undefined; textures?: string[]; wireframe?: boolean; distance?: number; number?: any; minDistance?: any; maxDistance?: any; }) {
@@ -85,11 +116,11 @@ class Asteriods extends Planet {
     }
     let points = new BufferGeometry()
     points.setAttribute("position", new Float32BufferAttribute(asteroidBuffer, 3))
-    this.mesh = new Points(points, new PointsMaterial({
+    const mesh = new Points(points, new PointsMaterial({
       color: 0xffffff,
       size: .5,
     }))
-    this.add(this.mesh)
+    this.add(mesh)
   }
   override update() { }
 }
@@ -98,4 +129,28 @@ export {
   Sun,
   Moon,
   Asteriods
+}
+
+export function createOrbitMesh(major: number, minor: number, segments: number = 100) {
+  const points = generateEllipsePoints(major, minor, segments)
+  const mesh = new BufferGeometry().setFromPoints(
+    points
+  )
+  return new LineLoop(mesh, new LineBasicMaterial({
+    color: 0xffffff
+  }))
+
+}
+function generateEllipsePoints(major: number, minor: number, segments: number = 100): Vector3[] {
+  const points = [];
+
+  for (let i = 0; i < segments; i++) {
+    const t = (i / (segments - 1)) * (2 * Math.PI);
+    const x = major * Math.cos(t);
+    const y = minor * Math.sin(t);
+
+    points.push(new Vector3(x, 0, y));
+  }
+
+  return points;
 }
