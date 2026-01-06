cf6f2faaa2e6555e2cceb81515e09daa544058a3
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-01-06 11:56:39 +0300

    Added command palette to focus on an object

--- src/scene.ts
+++ src/scene.ts
@@ -1,11 +1,15 @@
-import { AmbientLight, AxesHelper, PerspectiveCamera, Scene, Vector3, WebGLRenderer } from "three";
-import { Planet, Sun, Moon, createOrbitMesh, Asteriods } from "./planets.js";
+import { AmbientLight, AxesHelper, Object3D, PerspectiveCamera, Quaternion, Scene, Vector3, WebGLRenderer } from "three";
 import { FlyControls } from "./flycontrols.js";
+import { OrbitControls } from "./orbitcontrols.js";
+import { Asteriods, createOrbitMesh, Moon, Planet, Sun } from "./planets.js";
 
 class Demo extends Scene {
   renderer: WebGLRenderer;
+  commandElement: HTMLInputElement;
   camera: PerspectiveCamera;
+  focusedObject: Object3D | undefined;
   cameraController: FlyControls;
+  orbitControls: OrbitControls;
   animateNo: number = -1;
   lasttime = 0;
   speed = 0.01
@@ -13,12 +17,29 @@ class Demo extends Scene {
     super()
     this.renderer = new WebGLRenderer();
     this.camera = new PerspectiveCamera(50, 1, 0.1, 1000)
+    this.commandElement = document.getElementById('command-input') as HTMLInputElement
     this.cameraController = new FlyControls(this.camera, this.renderer.domElement)
+    this.orbitControls = new OrbitControls(this.camera, this.renderer.domElement)
+    this.orbitControls.listenToKeyEvents(this.renderer.domElement)
+    this.orbitControls.enablePan = false
+    this.renderer.domElement.tabIndex = 0
     this.children = []
+
     this.init()
     this._raf()
-    addEventListener('resize', () => this.onresize())
-    addEventListener('keydown', (e) => this.zoom(e))
+    this.commandElement.addEventListener('keydown', this.onCommand.bind(this))
+    addEventListener('resize', () => this.resize())
+    addEventListener('keydown', (event) => {
+      if(event.code === 'Slash'){
+        if(document.activeElement === this.commandElement){
+          this.renderer.domElement.focus()
+        } else {
+          this.commandElement.focus()
+        }
+        event.stopImmediatePropagation()
+        event.preventDefault()
+      }
+    })
   }
   init() {
     const light = new AmbientLight(0xffffff, 0)
@@ -27,12 +48,13 @@ class Demo extends Scene {
     this.loadUnits()
     this.camera.position.set(150, 150, 100)
     this.camera.lookAt(new Vector3(0, 0, 0))
-    this.renderer.setSize(innerWidth, innerHeight)
     this.renderer.shadowMap.enabled = true
     this.cameraController.dragToLook = true
     this.cameraController.rollSpeed = 0.1
+    this.orbitControls.enabled = false
     this.cameraController.movementSpeed = 5
     this.add(this.camera, light, axis)
+    this.resize()
     document.body.append(this.renderer.domElement)
   }
   async loadUnits() {
@@ -88,28 +110,73 @@ class Demo extends Scene {
   _pause() {
     cancelAnimationFrame(this.animateNo)
   }
-  onresize() {
-    this.camera.aspect = window.innerWidth / window.innerHeight
+  resize() {
+    const width = innerWidth
+    const height = innerHeight
+
+    this.camera.aspect = width / height
     this.camera.updateProjectionMatrix()
-    this.renderer.setSize(innerWidth, innerHeight);
+    this.renderer.setSize(width, height);
   }
   zoom(e: KeyboardEvent) {
-    let r = e.code.toLowerCase()
-    if (r.includes('key')) {
-      r = r.slice(3, e.code.length)
-    }
-    if (r == 'n') {
-      this.camera.zoom += 1
+    if (e.code == 'KeyN') {
       this.camera.updateProjectionMatrix()
     }
-    if (r == 'm' && this.camera.zoom > 1) {
+    if (e.code == 'KeyM' && this.camera.zoom > 1) {
       this.camera.zoom -= 1
       this.camera.updateProjectionMatrix()
     }
   }
 
+  focus(objectName: string) {
+    const object = this.getObjectByName(objectName)
+
+    if (!object) {
+      this.unfocus()
+      return
+    }
+    this.focusedObject = object
+    this.cameraController.enabled = false
+    this.orbitControls.enabled = true
+  }
+
+  unfocus() {
+    const position = new Vector3()
+    const orientation = new Quaternion()
+    const scale = new Vector3()
+    this.camera.matrixWorld.decompose(position, orientation, scale)
+    this.camera.removeFromParent()
+    this.camera.position.copy(position)
+    this.camera.quaternion.copy(orientation)
+    this.camera.scale.copy(scale)
+    this.cameraController.enabled = true
+    this.orbitControls.enabled = false
+  }
+
+  onCommand(event: KeyboardEvent) {
+    if (event.code === 'Enter') {
+      const objectName = this.commandElement.value.toLowerCase()
+
+      if (objectName === 'none') {
+        this.unfocus()
+      } else {
+        this.focus(objectName)
+      }
+      this.renderer.domElement.focus()
+      this.commandElement.blur()
+      this.commandElement.value = ''
+    }
+  }
   update(dt: number) {
+    if (this.focusedObject) {
+      this.orbitControls.target.copy(
+        this.focusedObject.getWorldPosition(new Vector3())
+      )
+    }
     this.cameraController.update(dt)
+    if (this.orbitControls.enabled) {
+      this.orbitControls.update(dt)
+    }
     this.children.forEach(c => {
       c.traverse(child => {
         if (child instanceof Sun) {
