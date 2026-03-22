6d46431ea2e6c4c70955e30e18027362cdfe7b3d
waynemwashuma <94756970+waynemwashuma@users.noreply.github.com>
2026-03-22 15:03:12 +0300

    Fis deployment config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
 
-export default defineConfig({
-  server: {
+export default defineConfig(({ command }) => {
+  const isBuild = command === 'build'
+
+  return {
     port: 8082,
     strictPort: true,
     open: "./index.html",
+    base: isBuild ? '/solar-system/' : '/'
   }
 })
